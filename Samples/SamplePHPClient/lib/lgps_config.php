8e3d148cff323d7c50115bbc105865d0197ef9f6
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-11-07 22:37:06 +0530

    initial commit

---
+++ Samples/SamplePHPClient/lib/lgps_config.php
@@ -0,0 +1,21 @@
+<?php
+
+define("LOG_DIRECTORY_PATH","C:/xampp/htdocs/LGPS_PHP_CLIENT/Samples/SamplePHPClient/logs");
+define("ENABLE_LOGS",true);
+//In WIndows:
+define("CLIENT_PUBLIC_KEY", "C:/xampp/htdocs/LGPS_PHP_CLIENT/Samples/keys/0001/test2-0001.crt");
+define("CLIENT_PRIVATE_KEY", "C:/xampp/htdocs/LGPS_PHP_CLIENT/Samples/keys/0001/test2-0001.key");
+define("CLIENT_KEY_PASSWORD", "test2-0001");
+define("LGPS_PUBLIC_KEY", "C:/xampp/htdocs/LGPS_PHP_CLIENT/Samples/keys/0001/testlgps.pem");
+// define("CLIENT_PUBLIC_KEY", "C:/lgps_keys/merchants/test2/test2-0001/test2-0001.crt");
+// define("CLIENT_PRIVATE_KEY", "C:/lgps_keys/merchants/test2/test2-0001/test2-0001.key");
+// define("CLIENT_KEY_PASSWORD", "test2-0001");
+// define("LGPS_PUBLIC_KEY", "C:/lgps_keys/merchants/test2/test2-0001/testlgps.pem");
+
+
+//In Linux:
+// define("CLIENT_PUBLIC_KEY", "/opt/lgps/keys/merchants/TEST2/0001/test2-0001.crt");
+// define("CLIENT_PRIVATE_KEY", "/opt/lgps/keys/merchants/TEST2/0001/test2-0001.key");
+// define("CLIENT_KEY_PASSWORD", "test2-0001");
+// define("LGPS_PUBLIC_KEY", "/opt/lgps/keys/merchants/TEST2/0001/testlgps.pem");
+?>
